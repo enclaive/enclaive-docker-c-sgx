7cfc4286e2907e1e344355f13c9530291a765733
Arvid E. Picciani <arvid@kraud.cloud>
2022-09-01 10:39:10 +0000

    demo

--- app/main.c
+++ app/main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+
 int main() {
-    printf("Hello World\n");
+    printf("going to look at your hosts kernel commandline:\n");
+
+    int fd = open("/proc/cmdline", O_RDONLY);
+    char buf[1024];
+    int buflen;
+    while((buflen = read(fd, buf, 1024)) > 0)
+    {
+        write(2, buf, buflen);
+    }
+    buf[0] = '\n';
+    write(2, buf, 1);
+
+    close(fd);
     return 0;
 }
